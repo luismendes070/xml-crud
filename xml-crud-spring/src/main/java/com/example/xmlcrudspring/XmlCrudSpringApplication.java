92e5b7c022bc716e1920b56d2a9b6cc0befb3f42
Luis Mendes <machadoluismendes@gmail.com>
2023-02-04 11:08:05 -0300

    Initial commit
    CRUD XML java 8
    spring initilizr
    angular cli

---
+++ xml-crud-spring/src/main/java/com/example/xmlcrudspring/XmlCrudSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.xmlcrudspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlCrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlCrudSpringApplication.class, args);
+	}
+
+}
---
+++ xml-crud-spring/src/test/java/com/example/xmlcrudspring/XmlCrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xmlcrudspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlCrudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
